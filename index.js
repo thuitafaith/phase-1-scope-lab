2bbf67e39bc5e6b47eab0333edb7af885aa905ed
thuitaf <faith.thuita@moringaschool.com>
2022-03-09 20:08:09 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName (){
+
+    customerName = customerName.toUpperCase();
+}
+
+// var bestCustomer = 'some initial value';
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer(){
+
+    leastFavoriteCustomer = 'bob'
+
+}
