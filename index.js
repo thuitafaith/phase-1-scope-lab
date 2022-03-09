ac9c40ca32615ea38639241c069a663851c72134
thuitaf <faith.thuita@moringaschool.com>
2022-03-09 20:00:05 +0300

    commit tree d5d285648f689b62061b16b5a30cd2db96968d34

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+var customerName;
+
+function upperCaseCustomerName (customerName){
+
+    return customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+}
